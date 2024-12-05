963c11da993b0c8e04f11c94ae0be8eb0d8cc910
Diego Alejandro Rubi <drubico@gmail.com>
2024-12-05 09:27:30 -0600

    ✨01-08-AuthContext-Enumeraciones y estado

--- 01-bases/src/components/LoginPage.tsx
+++ 01-bases/src/components/LoginPage.tsx
@@ -1,12 +1,20 @@
 import { useAuthcontext } from "../context/AuthContext";
 
 export const LoginPage = () => {
-	const { hola } = useAuthcontext();
+	const { isChecking } = useAuthcontext();
+
+	if (isChecking) {
+		return (
+			<div>
+				<h3>Login</h3>
+				<span>Checking...</span>
+			</div>
+		);
+	}
 
 	return (
 		<div>
 			<h3>Login</h3>
-			<span>{hola}</span>
 		</div>
 	);
 };
--- 01-bases/src/context/AuthContext.tsx
+++ 01-bases/src/context/AuthContext.tsx
@@ -1,7 +1,20 @@
-import { createContext, PropsWithChildren, useContext } from "react";
+import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
 
+enum AuthStatus {
+	"checking" = "checking",
+	"authenticated" = "authenticated",
+	"unauthenticated" = "unauthenticated",
+}
+
+interface User {
+	name: string;
+	email: string;
+}
 interface AuthState {
-	hola: string;
+	status: AuthStatus;
+	token?: string;
+	user?: User;
+	isChecking: boolean;
 }
 
 export const AuthContext = createContext({} as AuthState);
@@ -9,10 +22,21 @@ export const AuthContext = createContext({} as AuthState);
 export const useAuthcontext = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
+	const [status, setstatus] = useState(AuthStatus.checking);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setstatus(AuthStatus.unauthenticated);
+		}, 1500);
+	}, []);
+
 	return (
 		<AuthContext.Provider
 			value={{
-				hola: "Mundo",
+				status: status,
+
+				// Getter
+				isChecking: status === AuthStatus.checking,
 			}}
 		>
 			{children}
